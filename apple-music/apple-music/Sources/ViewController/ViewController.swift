5efdd125997ba79c3d4e220c9af55aa4cd777587
iamcho2 <thatgrass@naver.com>
2020-11-29 23:20:27 +0900

    fix typo

--- apple-music/apple-music/Sources/ViewController/ViewController.swift
+++ apple-music/apple-music/Sources/ViewController/ViewController.swift
@@ -113,7 +113,7 @@ extension ViewController: UITableViewDataSource{
         }else if(indexPath.row == 12) {
             return 305
         }else if(indexPath.row == 13) {
-            return 240g
+            return 240
         }else{
             return 0
         }
